9d238227e9ad79aafd4b38b389617a4e2871277d
masutcu <masutcu@gmail.com>
2023-06-05 12:48:50 +0300

    ui.ok

--- src/test/java/stepdefinitions/MedunnaSignInStepDefinitions.java
+++ src/test/java/stepdefinitions/MedunnaSignInStepDefinitions.java
@@ -1,9 +1,16 @@
 package stepdefinitions;
 
 import io.cucumber.java.en.*;
+import pages.MedunnaHomePage;
+import pages.MedunnaLoginPage;
+import pages.MedunnaRoomPage;
+import utilities.ConfigReader;
 import utilities.Driver;
 
-public class MedunnaSignInStepDefinitions {
+public class MedunnaSignInStepDefinitions  {
+    MedunnaHomePage medunnaHomePage = new MedunnaHomePage();
+    MedunnaLoginPage medunnaLoginPage = new MedunnaLoginPage();
+    MedunnaRoomPage medunnaRoomPage = new MedunnaRoomPage();
     @Given("go to {string}")
     public void go_to(String url) {
         Driver.getDriver().get(url);
@@ -11,22 +18,27 @@ public class MedunnaSignInStepDefinitions {
     }
     @When("click on user icon")
     public void click_on_user_icon() {
+        medunnaHomePage.userIcon.click();
 
     }
     @When("click on sign in option")
     public void click_on_sign_in_option() {
+        medunnaHomePage.signInOption.click();
 
     }
     @When("send user name into username input")
     public void send_user_name_into_username_input() {
+        medunnaLoginPage.usernameInput.sendKeys(ConfigReader.getProperty("medunna_username"));
 
     }
     @When("send password into password input")
     public void send_password_into_password_input() {
+        medunnaLoginPage.passwordInput.sendKeys(ConfigReader.getProperty("medunna_password"));
 
     }
     @When("click on sign in submit button")
     public void click_on_sign_in_submit_button() {
+        medunnaLoginPage.signInSubmitButton.click();
 
     }
 }
