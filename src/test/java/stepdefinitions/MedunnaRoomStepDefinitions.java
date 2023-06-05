b622b52e3f87536df571969f1971e1bbe6208acc
masutcu <masutcu@gmail.com>
2023-06-05 11:58:52 +0300

    e2estart

---
+++ src/test/java/stepdefinitions/MedunnaRoomStepDefinitions.java
@@ -0,0 +1,10 @@
+package stepdefinitions;
+
+import io.cucumber.java.en.When;
+
+public class MedunnaRoomStepDefinitions {
+    @When("Click on ItemsAndTitles")
+    public void click_on_ıtems_and_titles() {
+
+    }
+}
